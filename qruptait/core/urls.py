a77241ab08b4e6643d27b25d151ccc1bf3c2592a
henry5711 <henryrom57@gmail.com>
2023-11-15 11:25:40 -0400

    liesto

--- qruptait/core/urls.py
+++ qruptait/core/urls.py
@@ -3,11 +3,11 @@ from django.urls import path,include
 from . import views
 
 
-app_name = 'core'
+#app_name = 'core'
 urlpatterns = [
     path("",views.index,name="index"),
-    path('users',views.indexUser,name='users'),
-    path('asistencia',views.indexasistence),
+    path("users",views.indexUser, name="users"),
+    path('asistencia',views.indexasistence, name='asistencia'),
     path('generate_qr/<str:data>/', views.generate_qr, name='generate_qr'),
 ]
 
